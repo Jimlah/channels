a616dd689efd32ca9be608b8eec548568e15b8b8
Abdullahi Jimoh <abdullahij951@gmail.com>
2022-01-09 21:34:27 +0100

    Created the message methods

---
+++ src/SMSToMessage.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace NotificationChannels\SMSTo;
+
+use Illuminate\Support\Arr;
+
+class SMSToMessage
+{
+    public string $to;
+    public string $sender_id;
+    public array $lines;
+
+    public function __construct($notifiable)
+    {
+        $this->to = $notifiable->routeNotificationFor('SMSTo');
+    }
+
+    public function to(string $to): self
+    {
+        $this->to = $to;
+
+        return $this;
+    }
+
+    public function senderId(string $senderId): self
+    {
+        $this->sender_id = $senderId;
+
+        return $this;
+    }
+
+    public function lines(array $lines): self
+    {
+        $this->lines = $lines;
+
+        return $this;
+    }
+
+    public function line(string $line): self
+    {
+        $this->lines[] = $line;
+
+        return $this;
+    }
+}
